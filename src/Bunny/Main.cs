276f6132470b218344cdef2d5f177fe2954a989c
Matt <matt.janda.kingston@gmail.com>
2024-02-28 17:25:11 +0000

    Add send command implementation

--- src/Bunny/Main.cs
+++ src/Bunny/Main.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using System.Text;
 
 internal class Main
 {
@@ -14,13 +16,23 @@ internal class Main
     public void Send(string text)
     {
         var message = _config.Bind<Settings>("Bunny").Message;
-
         _logger.LogInformation(message, text);
+
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+        var body = Encoding.UTF8.GetBytes(text);
+
+        channel.BasicPublish(exchange: string.Empty, routingKey: "hello", basicProperties: null, body: body);
+
+        _logger.LogInformation("The message sent successfully.");
     }
 
     public void Receive()
     {
 
     }
-
 }
