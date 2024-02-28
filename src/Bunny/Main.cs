b3b58a67b2fee74a4d1cff323da6f2d35ba917db
Matt <matt.janda.kingston@gmail.com>
2024-02-28 17:29:18 +0000

    Add receive command implementation

--- src/Bunny/Main.cs
+++ src/Bunny/Main.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System.Text;
 
 internal class Main
@@ -33,6 +34,25 @@ internal class Main
 
     public void Receive()
     {
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
 
+        channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+        _logger.LogInformation("Waiting for messages...");
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            _logger.LogInformation("Received {message}", message);
+        };
+        channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+        _logger.LogInformation("Receive complete.");
     }
 }
